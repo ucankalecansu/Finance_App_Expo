b224713067a6b58cdcc1a7027465b961edd49fe3
Cansu Uçankale <54913401+ucankalecansu@users.noreply.github.com>
2022-02-12 22:38:51 +0300

    listItem component

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import ListItem from "./components/ListItem";
+
+import {SAMPLE_DATA} from "./assets/data/sampleData";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrapper}>
+        <Text style={styles.largeTitle}>Market</Text>
+      </View>
+      <View style={styles.divider}/>
+      <ListItem
+          name={SAMPLE_DATA[0].name}
+          symbol={SAMPLE_DATA[0].symbol}
+          currentPrice={SAMPLE_DATA[0].current_price}
+          priceChangePercentage7d={SAMPLE_DATA[0].price_change_percentage_7d_in_currency}
+          logoUrl={SAMPLE_DATA[0].image}
+      />
+
     </View>
   );
 }
@@ -14,7 +27,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  titleWrapper:{
+    marginTop:80,
+    paddingHorizontal:16
+  },
+  largeTitle:{
+    fontSize:24,
+    fontWeight:"bold",
+  },
+  divider:{
+    height:1,
+    backgroundColor:"#A9ABB1",
+    marginTop:16,
+
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {TouchableOpacity,Image,View,StyleSheet,Text} from "react-native";
+
+const ListItem = ({ name, symbol, currentPrice, priceChangePercentage7d, logoUrl, onPress }) => {
+    const priceChangeColor = priceChangePercentage7d > 0 ? '#34C759' : '#FF3B30';
+
+
+    return(
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.itemWrapper}>
+               <View style={styles.leftWrapper}>
+                   <Image source={{uri:logoUrl}} style={styles.image}/>
+                   <View style={styles.titlesWrapper}>
+                       <Text style={styles.title}>{name}</Text>
+                       <Text style={styles.subtitle}>{symbol.toUpperCase()}</Text>
+                   </View>
+               </View>
+
+                <View style={styles.rightWrapper}>
+                    <Text style={styles.title}>${currentPrice.toLocaleString('en-US',{currency:'USD'})}</Text>
+                    <Text style={[styles.subtitle, {color: priceChangeColor}]}>{priceChangePercentage7d.toFixed(2)}%</Text>
+                </View>
+            </View>
+
+        </TouchableOpacity>
+    )
+}
+
+const styles=StyleSheet.create({
+    itemWrapper:{
+        paddingHorizontal:16,
+        marginTop:24,
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+
+    },
+    leftWrapper:{
+        flexDirection:"row",
+        alignItems:"center",
+
+    },
+    image:{
+        height:48,
+        width:48,
+
+    },
+    titlesWrapper:{
+        marginLeft:8
+    },
+    title:{
+        fontSize:18
+    },
+    subtitle:{
+        marginTop:4,
+        fontSize:14,
+        color:"#A9ABB1"
+    },
+    rightWrapper:{
+        alignItems:"flex-end",
+    }
+})
+
+
+
+export default ListItem;
