c641df9ec3a8f91960d8508071eb09b19c0ffe66
Cansu Uçankale <54913401+ucankalecansu@users.noreply.github.com>
2022-02-12 22:42:12 +0300

    app flatlist data

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import ListItem from "./components/ListItem";
 
 import {SAMPLE_DATA} from "./assets/data/sampleData";
 
+const ListHeader=()=>{
+    return(
+        <>
+            <View style={styles.titleWrapper}>
+                <Text style={styles.largeTitle}>Market</Text>
+            </View>
+            <View style={styles.divider}/>
+        </>
+    )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.titleWrapper}>
-        <Text style={styles.largeTitle}>Market</Text>
-      </View>
-      <View style={styles.divider}/>
-      <ListItem
-          name={SAMPLE_DATA[0].name}
-          symbol={SAMPLE_DATA[0].symbol}
-          currentPrice={SAMPLE_DATA[0].current_price}
-          priceChangePercentage7d={SAMPLE_DATA[0].price_change_percentage_7d_in_currency}
-          logoUrl={SAMPLE_DATA[0].image}
+
+      <FlatList
+          keyExtractor={(item)=>item.id}
+          data={SAMPLE_DATA}
+          renderItem={({item})=>(
+              <ListItem
+                  name={item.name}
+                  symbol={item.symbol}
+                  currentPrice={item.current_price}
+                  priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+                  logoUrl={item.image}
+              />
+          )}
+          ListHeaderComponent={<ListHeader/>}
       />
 
     </View>
